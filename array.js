9506ea2667abcd0067a47cd9e34f455ecfddade0
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-02-13 10:25:30 -0300

    andamento no curso de javascript

---
+++ array.js
@@ -0,0 +1,15 @@
+/*
+const nota1 = 5;
+const nota2 = 10;
+const nota3 = 8;
+const nota4 = 6; 
+*/
+
+const notas = [5, 5, 5, 5, 7];
+
+//notas.push(7);
+notas.pop();
+
+const media = (notas[0] + notas[1] + notas[2] + notas [3]) / notas.length;
+
+console.log(media.toFixed(2));
