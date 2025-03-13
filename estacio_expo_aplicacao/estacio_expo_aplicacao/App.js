d2c4c0a95c2de61f3d7d7b707cf6f0fe4cf264b7
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-03-13 12:20:09 -0300

    Criando Projeto Fokus Node JS
    Node.js expo e React Native

--- estacio_expo_aplicacao/estacio_expo_aplicacao/App.js
+++ estacio_expo_aplicacao/estacio_expo_aplicacao/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar } from 'react-native';
+
+const Lista = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<SafeAreaView style={styles.safecontainer}>
+	  <ScrollView style={styles.containerScrollView}>
+		<Text style={styles.text}>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Pellentesque id dui sed nulla imperdiet scelerisque.
+					Integer malesuada facilisis nibh varius eleifend.
+					Cras a velit laoreet dui interdum consectetur.
+					Pellentesque volutpat placerat mauris in interdum.
+					Pellentesque non egestas sem. Suspendisse malesuada at augue
+					sit amet pretium.
+					Praesent odio nisl, semper vitae purus a, elementum ultrices arcu.
+					Praesent blandit lectus et aliquet posuere.
+					Nulla dictum, nisi id feugiat suscipit, mi sem maximus turpis,
+					vel aliquet massa ex sit amet sem.
+					Sed ullamcorper enim non elit vestibulum, feugiat euismod elit
+					consectetur. In et pulvinar eros.
+		</Text>
+	  </ScrollView>
+	</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safecontainer: {
+	flex: 1,
+	paddingTop: StatusBar.currentHeight,
+  },
+  containerScrollView: {
+	backgroundColor: 'grey',
+	marginHorizontal: 20,
+  },
+  text: {
+	fontSize: 26,
   },
 });
+
+export default Lista;
+
