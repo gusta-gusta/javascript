a26e529b3c7ce759c933e9d39cb643be06b5390d
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-03-13 15:14:29 -0300

    Projeto Fokus 30%

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import {Image, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+      <Image source={require('./pomodoro.png')}/>
+     <View style={styles.actions}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+  container:  
+            {
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: '#021123',
+              gap: 40
+
+            },
+
+  actions: 
+          	{
+              padding: 24,
+              backgroundColor: '#14448080',
+              width: '80%',
+              borderRadius: 32,
+              borderWidth: 2,
+              borderColor: '#144480',
+            }
+})
