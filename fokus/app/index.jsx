ecc81d20892e1db87c43a9fd10e63849e8b36381
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-03-21 11:48:23 -0300

    Finalizando Curso Fokus React Native

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -1,59 +1,108 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 import { FokusButton } from "../components/FokusButton";
 import { ActionButton } from "../components/ActionButton";
 import { Timer } from "../components/Timer";
+import { IconPause, IconPlay } from "../components/Icons";
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
+    initialValue: 25 * 60,
     image: require('./pomodoro.png'),
     display: 'Foco'
   },
   {
     id: 'short',
-    initialValue: 5,
+    initialValue: 5 * 60,
     image: require('./short.png'),
     display: 'Pausa curta'
   },
   {
     id: 'long',
-    initialValue: 15,
+    initialValue: 15 * 60,
     image: require('./long.png'),
     display: 'Pausa longa'
   },
 ]
 
 export default function Index() {
+  const [timerType, setTimerType] = useState(pomodoro[0]);
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue);
 
-  const [timerType, setTimerType] = useState(pomodoro[0])
+
+  const [timerRunning, setTimerRunning] = useState(false)
+
+  const timerRef = useRef(null);
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current)
+      timerRef.current = null
+      setTimerRunning(false)
+    }
+  }
+
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      // pausar
+      clear()   
+      return;
+    }
+
+    setTimerRunning(true)
+
+    const id = setInterval(() => {
+      setSeconds(oldState => {
+        if (oldState === 0) {
+          clear()
+          return timerType.initialValue
+        }
+        return oldState - 1
+      })
+
+      console.log('timer rolando');
+    }, 1000);
+    timerRef.current = id;
+  };
+
 
   return (
     <View
       style={styles.container}
     >
-      <Image source={timerType.image}/>
+      <Image source={timerType.image} />
       <View style={styles.actions}>
         <View style={styles.context}>
           {pomodoro.map(p => (
-            <ActionButton 
+            <ActionButton
               key={p.id}
-              active={ timerType.id === p.id }
-              onPress={() => setTimerType(p)}
+              active={timerType.id === p.id}
+              onPress={() => toggleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timerType.initialValue} />
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? 'Pausar' : 'Começar'}
+          icon={timerRunning ? <IconPause/> : <IconPlay/>}
+          onPress={toggleTimer}
+        />
+
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
         </Text>
         <Text style={styles.footerText}>
-          Desenvolvido por Alura. 
+          Desenvolvido por Alura.
         </Text>
       </View>
     </View>
--- fokus/components/FokusButton/index.jsx
+++ fokus/components/FokusButton/index.jsx
@@ -1,24 +1,33 @@
 import { Pressable, StyleSheet, Text } from "react-native"
 
-export const FokusButton = () => {
-    return (
-        <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>
-                Começar
-            </Text>
-        </Pressable>
-    )
+export const FokusButton = ({ onPress, title, icon }) => {
+  return (
+      <Pressable style={styles.button} onPress={onPress}>
+        {icon}
+          <Text style={styles.buttonText}>
+              {title}
+          </Text>
+      </Pressable>
+  )
 }
+
 
 const styles = StyleSheet.create({
   button: {
     backgroundColor: '#B872FF',
     borderRadius: 32,
-    padding: 8
+    padding: 8,
+    flexDirection: 'row',
+    gap: 12,
+    alinItems: 'center',
+    justifyContent: 'center',
   },
   buttonText: {
     textAlign: 'center',
     color: '#021123',
     fontSize: 18
   }
+
+
+
 })
